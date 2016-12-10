291d978b17fc5c98be69ac0c3509d1844fc8eeaa
Dan Liew <daniel.liew@imperial.ac.uk>
2016-12-10 18:48:31 +0000

    Comment out the `(int16_t)l == 0` assertion. It fails with GCC.

--- syn/ld-unnormal/test.c
+++ syn/ld-unnormal/test.c
@@ -111,7 +111,14 @@ int main(int argc, char **argv) {
   assert((uint8_t)l == 0);
   assert((int8_t)l == 0);
   assert((uint16_t)l == 0);
-  assert((int16_t)l == 0);
+
+  // GCC and Clangs behaviour differ here. GCC 6.2.1 (both at `-O0` and `-O2`)
+  // fail this assertion with the casted value being `-32768`. Clang passes
+  // this assertion.
+  // TODO: Report to Clang devs
+  // TODO: Report to GCC devs
+  // assert((int16_t)l == 0);
+
   assert((uint32_t)l == 0);
   assert((int32_t)l == -2147483648);
   assert((uint64_t)l == 0);

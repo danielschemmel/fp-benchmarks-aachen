d547e703d8b066bf28aaa21487c423c12b09521c
Dan Liew <daniel.liew@imperial.ac.uk>
2016-12-10 18:03:10 +0000

    Add function to dump the bits of a long double and also add a few comments on it.

--- syn/ld-unnormal/test.c
+++ syn/ld-unnormal/test.c
@@ -1,6 +1,15 @@
 #include <assert.h>
 #include <math.h>
 #include <stdint.h>
+#include <inttypes.h>
+#include <string.h>
+#include <stdio.h>
+
+void dump_long_double(char* name, long double ld) {
+  uint64_t data[2] = {0, 0};
+  memcpy(data, &ld, 10);
+  printf("%s: 0x%.4" PRIx16 " %.16" PRIx64 "\n", name, (uint16_t)data[1], data[0]);
+}
 
 int main(int argc, char **argv) {
   long double l = -5.0l;
@@ -8,6 +17,19 @@ int main(int argc, char **argv) {
   uint64_t *l_bits = (void *)&l;
 
   l_bits[0] &= 0x7FFFFFFFFFFFFFFF;
+  // l: 0xc001 2000000000000000
+  //
+  // Sign    Exponent        Significand
+  //                      Integer  Fraction
+  // [1] [100000000000001]  [0]    [010...0]
+  //
+  // This is part of the  "Negative Floating Point Unnormals"
+  // class as defined in 8.2.2 "Unsupported Double Extended-Precision
+  // Floating-Point Encodings and Pseduo-Denormals" in the Intel(R)
+  // 64 and IA-32 Architectures Software Developer's Manual.
+  //
+  // This is an invalid operand.
+  dump_long_double("l", l);
 
   assert(!isnan(l));
   assert(isfinite(l));

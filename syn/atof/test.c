28399e5c23b5fabbbdd579e861f95a537651a67d
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2016-12-22 01:12:06 +0100

    fix for syn/atof scientific notation

--- syn/atof/test.c
+++ syn/atof/test.c
@@ -1,3 +1,5 @@
+#include <stddef.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <assert.h>
 #include <klee/klee.h>
@@ -9,7 +11,11 @@ int main(int argc, char** argv)
 	input[0] = '0';
 	input[1] = '.';
 	input[sizeof(input) - 1] = 0;
+	bool has_e = false;
+	for(size_t i = 2; i < sizeof(input) - 1; ++i) {
+		if(input[i] == 'e' || input[i] == 'E') has_e = true;
+	}
 	double out = atof(input);
 	assert(out >= 0);
-	assert(out < 1);
+	assert(has_e || out < 1);
 }
